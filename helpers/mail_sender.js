b010116575053daf0e43318dd75fb9dddbfc97bf
sudeepkotari <sudeepkotari@gmail.com>
2021-09-18 08:23:12 +0530

    sendgrid mail added

--- helpers/mail_sender.js
+++ helpers/mail_sender.js
@@ -1,70 +1,82 @@
-const nodemailer = require("nodemailer");
-const createError = require("http-errors");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: "sudeepkotari@gmail.com", // Change to your recipient
+//   from: {
+//     name: "college-query",
+//     email: "viralitt@gmail.com",
+//   },
+//   subject: "Sending with SendGrid is Fun",
+//   text: "and easy to do anywhere, even with Node.js",
+//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+// };
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log("Email sent");
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+// const nodemailer = require("nodemailer");
+// const createError = require("http-errors");
 
 module.exports = {
   sendMail: (user, token) => {
     return new Promise((resolve, reject) => {
-      let mailTransporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        ignoreTLS: false,
-        secure: false,
-        auth: {
-          user: process.env.MAIL_ID,
-          pass: process.env.MAIL_PWD,
+      const msg = {
+        to: user, // Change to your recipient
+        from: {
+          name: "college-query",
+          email: "viralitt@gmail.com",
         },
-      });
-
-      let mailDetails = {
-        from: "college-query<viralitt@gmail.com>",
-        to: user,
-        subject: "Hello from College-Query",
-        text: "Hello from College-Query",
+        subject: "verification email from College-Query",
+        text: "verification email from College-Query",
         html: `<!DOCTYPEhtml>
-                <htm>
-                        <head>
-                        <style type="text/css">
-                            .btn {
-                                background-color:#3182CE;
-                                cursor:pointer;
-                                border-radius: 10px;
-                                height:50px;
-                                width:150px;
-                                color:#fff;
-                                border:none;
-                                font-size: 20px;
-                                font-weight: bold;
-                            }
-                        </style>
-                        </head>
-                        <body>
-                        <h1 style="text-align:center;">verify your email address</h1>
-                        <div style="text-align:center;">
-                        <a href='https://college-query.web.app/verify-mail/${token}'>
-                            <button class="btn">
-                                Confirm email
-                            </button>
-                        </a>
-                        </div>
-                        </body>
-                        </html>`,
+                      <htm>
+                              <head>
+                              <style type="text/css">
+                                  .btn {
+                                      background-color:#3182CE;
+                                      cursor:pointer;
+                                      border-radius: 10px;
+                                      height:50px;
+                                      width:150px;
+                                      color:#fff;
+                                      border:none;
+                                      font-size: 20px;
+                                      font-weight: bold;
+                                  }
+                              </style>
+                              </head>
+                              <body>
+                              <h1 style="text-align:center;">verify your email address</h1>
+                              <div style="text-align:center;">
+                              <a href='https://college-query.web.app/verify-mail/${token}'>
+                                  <button class="btn">
+                                      Confirm email
+                                  </button>
+                              </a>
+                              </div>
+                              </body>
+                              </html>`,
       };
 
-      mailTransporter.sendMail(mailDetails, function (err, data) {
-        if (err) {
-          console.log(err.message);
+      sgMail
+        .send(msg)
+        .then(() => {
+          const message =
+            "A verification link has been sent to your email account";
+          resolve(message);
+        })
+        .catch((error) => {
           reject(
             createError.InternalServerError(
               "gmail service not working try after some time"
             )
           );
-          return;
-        } else {
-          const message =
-            "A verification link has been sent to your email account";
-          resolve(message);
-        }
-      });
+        });
     });
   },
 };
